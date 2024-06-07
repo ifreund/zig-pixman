dc92d0ea4db457c8dfc2dda0baf18ee012af7d2a
Isaac Freund <mail@isaacfreund.com>
2024-06-07 12:36:47 +0200

    build: update to Zig version 0.13.0

--- build.zig
+++ build.zig
@@ -3,6 +3,6 @@ const Build = std.Build;
 
 pub fn build(b: *Build) !void {
     _ = b.addModule("pixman", .{
-        .root_source_file = .{ .path = "pixman.zig" },
+        .root_source_file = b.path("pixman.zig"),
     });
 }
